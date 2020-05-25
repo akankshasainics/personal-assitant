9c62463d309fba4a8e52f92b03cf737a4f320927
Akanksha Saini <akankshasaini03@gmail.com>
2020-05-25 21:11:51 +0530

    done note

--- task.py
+++ task.py
@@ -5,8 +5,8 @@ x = sys.argv
 string = x[2]
 
 
-def delete_note(conn, id):
-    sql = 'DELETE FROM notes WHERE id = ?'
+def delete_task(conn, id):
+    sql = 'DELETE FROM tasks WHERE id = ?'
     cur = conn.cursor()
     cur.execute(sql, (id,))
     conn.commit()
@@ -14,7 +14,7 @@ def delete_note(conn, id):
 
 def sql_fetch(con):
     cursorObj = con.cursor()
-    cursorObj.execute('SELECT * FROM notes')
+    cursorObj.execute('SELECT name FROM task')
     rows = cursorObj.fetchall()
     for row in rows:
         print(row)
@@ -37,34 +37,35 @@ def create_table(conn, create_table_sql):
         print(e)
 
 
-def create_note(conn, note):
-    sql = ''' INSERT INTO notes(name)
-           VALUES(?) '''
+def create_task(conn, task):
+    sql = ''' INSERT INTO tasks(name, ischecked)
+           VALUES(?, ?) '''
     cur = conn.cursor()
-    cur.execute(sql, note)
+    cur.execute(sql, task)
     return cur.lastrowid
 
 
 def main():
-    note_table = """ CREATE TABLE IF NOT EXISTS notes (
+    task_table = """ CREATE TABLE IF NOT EXISTS tasks (
                     id integer PRIMARY KEY AUTOINCREMENT,
-                    name text NOT NULL
+                    name text NOT NULL,
+                    is_checked BIT(0)
                 ); """
 
     conn = create_connection(r"pythonsqlite.db")
     if conn is not None:
-        create_table(conn, note_table)
+        create_table(conn, task_table)
     else:
         print("Error! cannot create the database connection.")
 
     with conn:
         if x[2] == "all":
             sql_fetch(conn)
-        elif x[2] == "delete":
+        elif x[2] == "clear":
             delete_note(conn, int(x[3]))
         else:
-            note = (x[2],)
-            create_note(conn, note)
+            task = (x[2],)
+            create_task(conn, task)
 
 
 if __name__ == '__main__':
